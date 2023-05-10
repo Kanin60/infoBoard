6077b305d6315c2f835e46dd312b7a61cad55fb3
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-05-10 09:15:23 +0200

    Færdig bus Api

--- assets/js/busstops.js
+++ assets/js/busstops.js
@@ -44,42 +44,17 @@ function buildBusData(data){
         let dataFive = data.splice(0,6);
         console.log('her er five',dataFive);
 
-        dataFive.map((data) => {
-            // console.log(data);
-            // buildBusCard(data);
-            // addParagraphs(data)
-        });
-
-        // function buildBusCard(data){
-        //     console.log('HER',data);
-        //     let busName = data.name.replace(/^Bybus\s/, '');
-        //     const bustider = document.getElementById("div5");
-        //     bustider.innerHTML =`<h2>Bustider: </h2>`;
-        //     const bus = document.getElementById("bus");
-        //     document.querySelector('bus').innerHTML = `<p>${busName} ${data.direction} ${data.time}</p>`;
-        // }
-        // function addParagraphs(data)
-        // {
-        //     var para = document.createElement("p");
-        //     let busName = data.name.replace(/^Bybus\s/, '');
-        //     let dataOne = `${busName} ${data.direction} ${data.time}`;
-        //     document.write(text.dataOne);
-        //     var node = document.createTextNode("paragraph 2");
-        //     para.appendChild(node);
-        //     var element = document.getElementById("p2");
-        //     element.appendChild(para);
-        // }
         const busDataContainer = document.getElementById('busDataContainer');
 
         dataFive.forEach(bus => {
-        const busName = bus.name.replace(/^Bybus\s/, '');
-        const direction = bus.direction;
-        const time = bus.time;
-    
-        const busDataParagraph = document.createElement('p');
-        busDataParagraph.textContent = `${busName} ${direction} ${time}`;
-    
-        busDataContainer.appendChild(busDataParagraph);
+            const busName = bus.name.replace(/^Bybus\s/, '');
+            const direction = bus.direction;
+            const time = bus.time;
+            const busDataParagraph = document.createElement('p');
+            
+            busDataParagraph.textContent = `${busName} ${direction} ${time}`;
+        
+            busDataContainer.appendChild(busDataParagraph);
         });
     }
 
